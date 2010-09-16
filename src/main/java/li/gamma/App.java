00fafb433650d424d9295a75ffe0c1feb522afbf
RG <ramon@gamma.li>
2010-09-16 20:55:24 +0200

    test

---
+++ src/main/java/li/gamma/App.java
@@ -0,0 +1,13 @@
+package li.gamma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
